127a44c39c421dd784bc0bb0f7f34db949b8bd58
Zeeshan <zeeshik62.zk@gmail.com>
2023-07-20 12:07:56 +0500

    Form Handling and Basic Form Validation

--- myapp/src/pages/Login.jsx
+++ myapp/src/pages/Login.jsx
@@ -1,22 +1,47 @@
+import { useState } from "react"
+
 const Login=()=>{
+
+    const [email,setEmail]=useState('');
+    const [pass,setPass]=useState('');
+
+
+    const handleEmail= (event)=>
+    {
+        setEmail(event.target.value)
+    }
+    const handlePassword= (event)=>
+    {
+        setPass(event.target.value)
+    }
+    const handleSubmit= (event)=>
+    {
+        event.preventDefault();
+        alert(`Email: ${email} \nPassword: ${pass}`)
+        
+    }
+
+
+
     return(
         <div>
-            <form>
-  <div class="row mb-3">
-    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-    <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3"/>
+            <form onSubmit={handleSubmit}>
+  <div className="row mb-3">
+    <label for="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+    <div className="col-sm-10">
+      <input type="email" onChange={handleEmail} className="form-control" id="inputEmail3"/>
+      
     </div>
   </div>
-  <div class="row mb-3">
-    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3"/>
+  <div className="row mb-3">
+    <label for="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+    <div className="col-sm-10">
+      <input type="password" onChange={handlePassword} className="form-control" id="inputPassword3"/>
     </div>
   </div>
   
  
-  <button type="submit" class="btn btn-primary">Sign in</button>
+  <button type="submit" className="btn btn-primary">Sign in</button>
 </form>
         </div>
     )
